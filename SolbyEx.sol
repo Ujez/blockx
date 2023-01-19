5363ff3f2d5c64e33b07a43e69550e13e96694d1
ujez <ujezujez@gmail.com>
2023-01-19 16:58:37 +0100

    the use of struct and enums

--- SolbyEx.sol
+++ SolbyEx.sol
@@ -210,32 +210,97 @@ pragma solidity ^0.8.7;
 //     }
 
 // }
-contract IterableMapping {
-    mapping(address => uint256) public balances;
-    mapping(address => bool) public inserted;
-    address[] public keys;
+// contract IterableMapping {
+//     mapping(address => uint256) public balances;
+//     mapping(address => bool) public inserted;
+//     address[] public keys;
 
-    function set(address _key, uint256 _val) external {
-        balances[_key] = _val;
-        if (!inserted[_key]) {
-            inserted[_key] = true;
-            keys.push(_key);
-        }
+//     function set(address _key, uint256 _val) external {
+//         balances[_key] = _val;
+//         if (!inserted[_key]) {
+//             inserted[_key] = true;
+//             keys.push(_key);
+//         }
+//     }
+
+//     function getSize() external view returns (uint256) {
+//         return keys.length;
+//     }
+
+//     function first() external view returns (uint256) {
+//         return balances[keys[0]];
+//     }
+
+//     function last() external view returns (uint256) {
+//         return balances[keys[keys.length - 1]];
+//     }
+
+//     function get(uint256 _i) external view returns (uint256) {
+//         return balances[keys[_i]];
+//     }
+// }
+
+// contract Structs {
+//     struct Car {
+//         string model;
+//         uint256 year;
+//         address owner;
+//     }
+//     Car public car; //using the struct as a state variable
+//     Car[] public cars;
+//     mapping(address => Car[]) public carsByOwner;
+
+//     function examples() external {
+//         Car memory toyota = Car("Toyota", 1990, msg.sender);
+//         Car memory lambo = Car({
+//             year: 1980,
+//             model: "Lamborghini",
+//             owner: msg.sender
+//         });
+//         Car memory tesla;
+//         tesla.model = "Tesla";
+//         tesla.year = 2010;
+//         tesla.owner = msg.sender;
+
+//         cars.push(toyota);
+//         cars.push(lambo);
+//         cars.push(tesla);
+
+//         cars.push(Car("Ferrari", 2020, msg.sender));
+
+//         Car storage _car = cars[0];
+//         _car.year = 1999;
+//         delete _car.owner;
+//         delete cars[1];
+//     }
+// }
+
+//USING ENUMS
+contract Enum{
+    enum Status{
+        None,
+        Pending,
+        Shipped,
+        Completed,
+        Rejected,
+        Canceled
     }
-
-    function getSize() external view returns (uint256) {
-        return keys.length;
+    Status public status;
+    struct Order{
+        address buyer;
+        Status status;
     }
-
-    function first() external view returns (uint256) {
-        return balances[keys[0]];
+    Order[] public orders;
+    function get() view external returns (Status) {
+        return status;
     }
-
-    function last() external view returns (uint256) {
-        return balances[keys[keys.length - 1]];
+    function set(Status _status) external {
+        status = _status;
     }
-
-    function get(uint256 _i) external view returns (uint256) {
-        return balances[keys[_i]];
+    function ship() external {
+        status = Status.Shipped;
+    }
+    function reset() external {
+        delete status;
     }
 }
